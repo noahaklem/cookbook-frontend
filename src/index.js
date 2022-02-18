a3935263a8235b08e4215261d438f042e24032dc
Noah A Klem <noahaklem@gmail.com>
2022-02-17 20:31:31 -0700

    render index data through index fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("in index")
-})
+  const endPoint = 'http://localhost:3000/api/v1/notes';
+  fetch(endPoint)
+  .then(res => res.json())
+  .then(recipeJson => 
+    recipeJson.forEach(recipe => {
+      const markup = `
+        <div>
+          <h3>${recipe.name}</h3>
+          <p>${recipe.cook_time}</p>
+          <button>X</button>
+        </div>
+      `
+      
+      document.querySelector("#recipe-container").innerHTML += markup;
+    })
+  );
+});
